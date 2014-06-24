3f24baf31a846130424809f94d45072f3fcb5986
Ryan Pfister <ryan.pfister@parivedasolutions.com>
2014-06-24 11:47:13 -0400

    Add new files and base functionality

---
+++ tip.js
@@ -0,0 +1,6 @@
+"use strict";
+
+function calculateTip(baseAmount) {
+	return 5;
+}
+
---
+++ total.js
@@ -0,0 +1,6 @@
+"use strict";
+
+//Adds base and tip together to produce total
+function calculateTotal(baseAmount, tip) {
+	return 10;
+}
